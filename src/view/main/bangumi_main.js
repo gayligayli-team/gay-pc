c67b8420a9998561509e564ce73804b1ef7fc510
kinght47 <839191843@qq.com>
2018-06-08 19:17:58 +0800

    番剧页面update

--- src/view/main/bangumi_main.js
+++ src/view/main/bangumi_main.js
@@ -29,6 +29,19 @@ class BangumiMain extends Component{
 	constructor(props){
 		super(props)
 		this.state = {
+			bangumi_tag: [
+				'搞笑',
+				'古风',
+				'泡面',
+				'漫改',
+			],
+			menu: [
+				'作品详情',
+				'长评',
+				'短评',
+				'相关视频',
+			],
+			score: 9.5,
 		}
 	}
 	componentDidMount(){
@@ -38,11 +51,66 @@ class BangumiMain extends Component{
 	render(){
 		return (
 			<div>
-				<div className="bangumi_nav">
-					
+				<div className="bangumi_nav clear">
+					<div className="fl">
+						<img src="" alt="" />
+					</div>
+					<div className="fl">
+						<p className="title">
+							<span>title</span>
+							{this.state.bangumi_tag.map((child, index) => (
+								<span className=""
+									 index={index}
+									 key={index}>{child}</span>
+							))}
+						</p>
+						<p className="title">
+							<span>
+								<em>总播放</em>
+								<em>x万</em>
+							</span>
+							<span>
+								<em>追番人数</em>
+								<em>x万</em>
+							</span>
+							<span>
+								<em>弹幕总数</em>
+								<em>x万</em>
+							</span>
+						</p>
+						<p className="title">
+							<span>20xx年x月x日 开播</span>
+							<span>连载中, 每周三、周六xx:xx更新</span>
+						</p>
+						<p>简介：【支付</p>
+						<p>
+							{/*<Button {...this.state.style} click={this.btn} />*/}
+						</p>
+						<div>
+							<p>
+								<span>{this.state.score}</span>
+								{[1,2,3,4,5].map((child, index) => (
+									<i className=""
+										 index={index}
+										 key={index}></i>
+								))}
+							</p>
+							<p className="comment">
+								<span>xx人评</span>|
+								<span><em>点评一下</em><i></i></span>
+							</p>
+						</div>
+					</div>
 				</div>
 				<div className="bangumi_menu">
-					
+					<ul>
+						{this.state.menu.map((child, index) => (
+							<li className=""
+								 index={index}
+								 key={index}
+								 onClick={this.changeParamsTime}>{child}</li>
+						))}
+					</ul>
 				</div>
 				<div className="bangumi_main clear">
 					<div className="fl"></div>
